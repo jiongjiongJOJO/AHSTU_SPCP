abc6ccfa80dad766fc1d1ade5928e77f6bd89575
囧囧JOJO <58727904+jiongjiongJOJO@users.noreply.github.com>
2021-02-18 19:26:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ codeInput = ''
 for i in range(4):
     codeInput += selectChar[random.randint(0, 54)]
 # 登录
-headers = {'Connection': 'close', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1',
+headers = {'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1',
            'Origin': 'http://xgb.ahstu.edu.cn', 'Content-Type': 'application/x-www-form-urlencoded',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.70',
            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -42,13 +42,13 @@ def Temper(time):
         date = ['8', '2']
     # 随机体温
     Temper = random.randint(0, 9)
-    headers = {'Connection': 'close', 'Pragma': 'no-cache', 'Cache-Control': 'no-cache',
+    headers = {'Connection': 'keep-alive', 'Cache-Control': 'max-age=0','Accept-Encoding':'gzip, deflate'
                'Upgrade-Insecure-Requests': '1', 'Origin': 'http://xgb.ahstu.edu.cn',
                'Content-Type': 'application/x-www-form-urlencoded',
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 Edg/85.0.564.70',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
                'Referer': 'http://xgb.ahstu.edu.cn/SPCP/Web/Temperature/StuTemperatureInfo',
-               'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6'}
+               'Accept-Language': 'zh-CN,zh;q=0.9'}
     data = {'TimeNowHour': date[0], 'TimeNowMinute': date[1], 'Temper1': '36', 'Temper2': Temper}
     response = requests.post('http://xgb.ahstu.edu.cn/SPCP/Web/Temperature/StuTemperatureInfo', headers=headers,
                              cookies=cookies, data=data)
@@ -66,7 +66,7 @@ for i in range(time_temper):
 
 # 疫情填报
 def yiqing():
-    headers = {'Connection': 'close', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1',
+    headers = {'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1',
                'Origin': 'http://xgb.ahstu.edu.cn', 'Content-Type': 'application/x-www-form-urlencoded',
                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
